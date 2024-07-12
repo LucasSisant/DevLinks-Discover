ea317eebdbf7ac5bbc7b6b64588d95c62533b839
Lucas Sisant <lucas_santana08@hotmail.com>
2024-07-12 03:47:20 -0300

    script for change profile img

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function toggleMode() {
   let html = document.documentElement
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img");
+  if(html.classList.contains("light")){
+    img.setAttribute("src", "./assets/avatarLucas-light.png")
+    img.setAttribute(
+      "alt", "Foto de Lucas usando boné virado para trás, oculos escuro e camisa cinza com a paisagem de uma praia com vegetação ao fundo."
+    )
+  }else{
+    img.setAttribute("src", "./assets/avatarLucas.png")
+    img.setAttribute(
+      "alt", "Foto de Lucas usando boné branco e camiseta branca com a paisagem de uma praia ao fundo."
+    )
+  }
 }
